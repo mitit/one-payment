b029fc7233fc9afe89d52f57b08c27883a74a445
Maksim Zaitsev <max.zaicev@gmail.com>
2020-03-15 23:59:15 +0100

    Deleted scrollback button, html design

--- one-payment-fe/src/components/GeneratedQr.vue
+++ one-payment-fe/src/components/GeneratedQr.vue
@@ -36,7 +36,7 @@
                 </div>
             </footer>
         </div>
-        <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
+        </div>
 </template>
 
 <script>
--- one-payment-fe/src/components/Payment.vue
+++ one-payment-fe/src/components/Payment.vue
@@ -62,7 +62,7 @@
                     </div>
                 </footer>
             </div>
-            <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
+            </div>
     </div>
 </template>
 
